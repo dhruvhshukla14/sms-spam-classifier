3d32a0cb9357be58218dcc61b90e4c3b506cceeb
dhruvhshukla14 <156407150+dhruvhshukla14@users.noreply.github.com>
2024-01-12 12:37:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("SMS Spam Classifier")
+
+input_sms = st.text_area("Please enter the message here")
+
+if st.button('Predict'):
+    if not input_sms.strip():  # Check if input_sms is empty or only contains whitespace
+        st.warning("Message is empty. Please enter a message.")
+    else:
+        # 1. preprocess
+        transformed_sms = transform_text(input_sms)
+        # 2. vectorize
+        vector_input = tfidf.transform([transformed_sms])
+        # 3. predict
+        result = model.predict(vector_input)[0]
+        # 4. Display
+        if result == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
+
+footer_html = """
+    <div style="position: fixed; bottom: 0; left: 0; right: 0; background-color: #f1f1f1; text-align: center; padding: 10px;">
+        Made by Dhruv Shukla.
+    </div>
+"""
+st.markdown(footer_html, unsafe_allow_html=True)
